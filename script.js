17ab41a797835edbeb33ea681ede5e1ffdaf0807
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-06 11:30:08 -0500

    draw circle and draw text function done

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const canvas = document.getElementById("pong");
 
-canvas.getContext("2d")
+const ctx = canvas.getContext("2d")
 
-const drawRect = (x, y,w,h, color) {
+const drawRect = (x, y,w,h, color) => {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, w, h);
 }
+
+const drawText = (text, x, y, color) => {
+    ctx.fillStyle = color;
+    ctx.font = "75px fantasy"
+    ctx.fillText(text, x, y)
+}
+
+const drawCircle = (x, y, r, color) => {
+    ctx.fillStyle = color;
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, Math.PI*2, true)
+    ctx.closePath()
+    ctx.fill()
+}
+
