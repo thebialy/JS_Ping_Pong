54cb1a193d5a3a62d488fc5322b9a2f0cea33ba6
thebialy <67696052+thebialy@users.noreply.github.com>
2021-05-13 15:21:37 -0500

    finished other part of collision function

--- script.js
+++ script.js
@@ -76,6 +76,13 @@ const collision = (b,p) {
     p.bottom = p.y + p.height;
     p.left = p.x;
     p.right = p.x + p.width;
+
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius
+
+    return p.left < b.right && p.top < b.bottom && p.right > b.left && p.bottom > b.top;
 }
 
 
